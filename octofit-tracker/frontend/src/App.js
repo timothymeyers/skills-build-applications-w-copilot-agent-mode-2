87cb5ce00e45f4756f67d8c6edb0b13d9c864a76
Tim <timothymeyers@users.noreply.github.com>
2025-04-09 19:05:39 +0000

    yes it is working

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">
               <img src={octoLogo} alt="OctoFit Logo" className="app-logo" />
